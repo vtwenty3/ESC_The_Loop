32b09aad8a109107c51ff4967179bd1572704108
vkisimov <vkisimov@podfather.com>
2024-06-12 16:36:06 +0100

    fix npm packages and modal expired delete

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import ModalExpired from './src/components/ModalExpired';
-import {name as appName} from './app.json';
+import {AppRegistry} from 'react-native'
+import App from './App'
+import {name as appName} from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
-//AppRegistry.registerComponent(modalComp, () => ModalExpired);
+AppRegistry.registerComponent(appName, () => App)
