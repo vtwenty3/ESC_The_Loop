7f435c4deb48f488da1fac1712315b8388357a94
kisimoff <kisimovvalentin@gmail.com>
2024-07-27 20:21:13 +0100

    it aint gonna happen ;(

--- expo/app/(tabs)/index.tsx
+++ expo/app/(tabs)/index.tsx
@@ -5,30 +5,59 @@ import {
   PermissionsAndroid,
   Linking,
   Platform,
-  Button
+  Button, Text
 } from 'react-native'
-import BackgroundService from 'react-native-background-actions'
-
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { currentActivity } from '@/modules/usage-stats';
-import { toggleBackground } from '@/hooks/useCurrentActivity'
 import { useEffect } from 'react';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import React from 'react';
+
+
+
+const BACKGROUND_TASK_NAME = 'BACKGROUND_TASK';
+
 
 export default function HomeScreen() {
 
+
+
+
+
   useEffect(() => {
-    if (!BackgroundService.isRunning()) {
-      toggleBackground()
-    }
-    // setAppUsageData()
-    // return () => {
-      // Remove AppState listener
-      // appStateListener.remove()
-    // }
-  }, [])
+    const initBackgroundFetch = async () => {
+      const status = await BackgroundFetch.getStatusAsync();
+      switch (status) {
+        case BackgroundFetch.Status.Restricted:
+        case BackgroundFetch.Status.Denied:
+          console.log('Background fetch is restricted or denied');
+          return;
+        default: {
+          console.log('Background fetch is enabled');
+          const isTaskDefined = TaskManager.isTaskDefined(BACKGROUND_TASK_NAME);
+          if (!isTaskDefined) {
+            console.log('Task is not defined');
+            return;
+          }
+
+          await BackgroundFetch.registerTaskAsync(BACKGROUND_TASK_NAME, {
+            minimumInterval: 2, // 2 seconds
+            stopOnTerminate: false, // continue on app close
+            startOnBoot: true, // start on device boot
+          });
+
+          console.log('Background fetch initialized');
+        }
+      }
+    };
+
+    initBackgroundFetch();
+  }, []);
+
 
   async function logActivity(){
     const ca = await currentActivity() 
@@ -36,23 +65,23 @@ export default function HomeScreen() {
 
   }
   async function openSettings() {
-    Linking.sendIntent('android.settings.USAGE_ACCESS_SETTINGS')
+    // Linking.sendIntent('android.settings.USAGE_ACCESS_SETTINGS')
 
 
-    // const pNotification = await PermissionsAndroid.check(
-    //   PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
-    // )
+    const pNotification = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    )
   
-    // if (pNotification) {
-    //   Linking.sendIntent('android.settings.USAGE_ACCESS_SETTINGS')
-    // } else {
-    //   await PermissionsAndroid.request(
-    //     PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
-    //   )
-    // }
+    if (pNotification) {
+      Linking.sendIntent('android.settings.USAGE_ACCESS_SETTINGS')
+    } else {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      )
+    }
   }
 
-
+console.log('normal')
 
   return (
     <ParallaxScrollView
@@ -92,4 +121,18 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer: {
+    backgroundColor:'white',
+    margin: 10,
+    padding: 20
+  },
+  boldText: {
+
+    fontWeight: 'bold',
+  }
 });
--- expo/hooks/useCurrentActivity.ts
+++ expo/hooks/useCurrentActivity.ts
@@ -1,8 +1,7 @@
-import {Parameters, Options, Timers} from '@/hooks/types'
-import BackgroundService from 'react-native-background-actions'
-import { isAfter, setHours, setMinutes, setSeconds, startOfTomorrow } from 'date-fns'
 import { currentActivity } from '@/modules/usage-stats';
 
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
 
 const debugLogs = true //reload the app on change
 
@@ -18,98 +17,23 @@ export async function fetchCurrentActivity() {
 }
 
 
-const sleep = (time: number) => new Promise<void>((resolve) => setTimeout(() => resolve(), time))
-export let iteration = 0
 
-export async function backgroundTimerTask(
-  index: number,
-  backgroundTaskParams?: Parameters,
-  options?: Options
-): Promise<void> {
-  if (options !== undefined && options !== null) {
-    debugLogs && console.log('in options')
-    backgroundTaskParams = {
-      delay: options.parameters.delay,
-      screenOffDelay: options.parameters.screenOffDelay,
-      timerExpiredDelay: options.parameters.timerExpiredDelay,
-    }
+const BACKGROUND_TASK_NAME = 'BACKGROUND_TASK';
+
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async () => {
+  try {
+    console.log('Background task running...');
+    // Your task execution logic here
+
+    return BackgroundFetch.BackgroundFetchResult.NewData;
+  } catch (error) {
+    console.log('Background task error:', error);
+    return BackgroundFetch.BackgroundFetchResult.Failed;
   }
-//   let userAlerted = false
-  let iterationCount = 0
-//   const localOptions = await localStorage.getOptions()
+});
 
-//   debugLogs && console.log('\n \n local background options:  ', localOptions)
-  debugLogs && console.log('\n \n background task params:  ', backgroundTaskParams)
-  debugLogs && console.log('Background Service On:', BackgroundService.isRunning(), ' Every: ', backgroundTaskParams!.delay)
 
-  for (let i = 0; BackgroundService.isRunning(); i++) {
-    if (index !== iteration) {
-      console.log('[This should stop the duplicate function] Stopping outdated background task', { index, iteration })
-      return 
-    }
-    iterationCount++
 
-    const currentActivity = await fetchCurrentActivity()
-    if (currentActivity === 'Screen Off!') {
-      await sleep(backgroundTaskParams!.screenOffDelay)
-      debugLogs && console.log('Screen Off. Waiting:', backgroundTaskParams!.screenOffDelay)
-    } else {
-    //   const trackedTimers = await localStorage.getDataByKey('@local_timers') as Timers // Fetch the latest timers from storage
-    BackgroundService.updateNotification({
-        taskTitle: 'ESC The Loop',
-        taskDesc: 'Current task is not timed.',
-        progressBar: undefined,
-      })
-      debugLogs && console.log('Current Activity:', currentActivity, { index, iteration })
-      await sleep(backgroundTaskParams!.delay)
-    }
-  }
-}
-
-export const defaultOptions: Options = {
-    taskName: 'ESC The Loop Background Service',
-    taskTitle: 'ESC The Loop',
-    taskDesc: 'Current task is not timed.',
-  
-    taskIcon: {
-      name: 'ic_launcher',
-      type: 'mipmap',
-    },
-  
-    linkingURI: 'escapetheloop://',
-  
-    parameters: {
-      delay: 2000,
-      screenOffDelay: 10000,
-      timerExpiredDelay: 10000,
-    },
-  }
-
-export async function toggleBackground() {
-  if (!BackgroundService.isRunning()) {
-    try {
-      iteration++
-    //   const localOptions = await localStorage.getOptions()
-
-      // Start the background service with the current iteration
-      // Bind the iteration to ensure the background task stops correctly if it's outdated
-      await BackgroundService.start(backgroundTimerTask.bind(null, iteration), defaultOptions)
-
-      if (debugLogs) console.log('Successful start!')
-    } catch (e) {
-      if (debugLogs) console.log('Error', e)
-    }
-    return true
-  } else {
-    try {
-      if (debugLogs) console.log('Stop background service')
-      await BackgroundService.stop()
-    } catch (e) {
-      if (debugLogs) console.log('Error', e)
-    }
-    return false
-  }
-}
 
 
 
